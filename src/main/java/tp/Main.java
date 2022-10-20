c8c067e934b7cb2bbcc1d87035f221cf912e8154
milemunoz10 <101755881+milemunoz10@users.noreply.github.com>
2022-10-20 15:46:50 -0300

    Update Main.java
    Creamos nuestro Main

--- src/main/java/tp/Main.java
+++ src/main/java/tp/Main.java
@@ -1,40 +1,17 @@
 package tp;
 
 public class Main {
-   /* public void listar (List<Dog> lista){
-        for(Dog d : lista){
-            System.out´p+println(d.getName());
-        }
-    }*/
+   
     public static void main(String[] args){
-
-        //EpicDoubleHashMap<Integer, String, String> map = new EpicDoubleHashMap<>();
-       // map.addFirstType(5,"Bulldog");
-        //map.addSecondType(2,"Bull Terrier");
-        //map.addTwo(1,"Chihuahua", "Pitbull");
-        //System.out.println(map.getSecondMap2(5));
-        /*System.out.println("Trabajo Practico 2");
-
-        System.out.println(new EpicDoubleHashMap());
-
-        Main ds = new Main();
-
-        Dog d1 = new Dog("Pepe","Chihuahua", "Jhon");
-        Dog d2 = new Dog("Rocco","Bulldog","Magui");
-        Dog d3 = new Dog("Rafa","Caniche", "Mary");
-
-        List<Dog> lista = new ArrayList<>();
-        lista.add(d1);
-        lista.add(d2);
-        lista.add(d3);
-
-        ds.listar(lista);
-
-*/
-
-
+       
+        System.out.println("Trabajo Practico 2");
+        EpicDoubleHashMap<Integer, String, String> map = new EpicDoubleHashMap<>();
+        map.addFirstType(5,"Bulldog");
+        map.addSecondType(2,"Bull Terrier");
+        map.addTwo(1,"Chihuahua", "Pitbull");
+        System.out.println(map.getSecondMap2(5));
+           
     }
-
 
 }
 
